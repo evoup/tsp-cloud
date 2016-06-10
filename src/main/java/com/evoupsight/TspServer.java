69770ec7df1a0943aec11867bb1d67cb78a9f066
evoup <evoex123@gmail.com>
2016-06-10 16:58:29 +0800

    main socket server logic

--- src/main/java/com/evoupsight/TspServer.java
+++ src/main/java/com/evoupsight/TspServer.java
@@ -1,13 +1,29 @@
 package com.evoupsight;
 
+import com.evoupsight.services.TspService;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
- * Hello world!
+ * TSP服务器
  *
  */
 public class TspServer
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            System.out.println("TSP server start at port 8888");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("client connected");
+                TspService myService = new TspService(socket);
+            }
+        } catch (IOException e) {
+            System.err.println("TSP server, error:" + e);
+        }
     }
 }
---
+++ src/main/java/com/evoupsight/services/TspService.java
@@ -0,0 +1,32 @@
+package com.evoupsight.services;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+/**
+ * Created by evoup on 16-6-10.
+ */
+public class TspService extends Thread {
+
+    DataInputStream dis;
+    DataOutputStream dos;
+
+    public TspService(Socket socket) {
+        try {
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            start();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void run() {
+        //Code
+        System.out.println("running");
+    }
+
+
+}
