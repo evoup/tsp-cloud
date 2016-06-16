251b8954be9f2f9600e94c3b8b81072377703c72
evoup <evoex123@gmail.com>
2016-06-16 22:05:46 +0800

    reduce

--- src/main/java/com/evoupsight/MyReduce.java
+++ src/main/java/com/evoupsight/MyReduce.java
@@ -1,5 +1,6 @@
 package com.evoupsight;
 
+import com.evoupsight.domain.TSPChromosome;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.OutputCollector;
@@ -13,7 +14,21 @@ import java.util.Iterator;
  * Created by evoup on 16-6-12.
  */
 public class MyReduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-    public void reduce(Text text, Iterator<Text> iterator, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
-
+    public void reduce(Text key, Iterator<Text> value, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
+        int minValue = 100000000;
+        String minGeneStr = "";
+        //取得所有map运算的结果，将最小适应度作为最优解
+        while (value.hasNext()) {
+            // 取得染色体字符串
+            String geneStr = value.next().toString();
+            // 计算适应度
+            // 判断是否是较好的适应度
+            if (TSPChromosome.caculateFitnessValue(geneStr) < minValue) {
+                minValue = TSPChromosome.caculateFitnessValue(geneStr);
+                minGeneStr = geneStr;
+            }
+        }
+        // 存储在负责封装输出数据的对象中
+        outputCollector.collect(new Text("TSPSolution"), new Text("result:" + minGeneStr + "_" + minValue));
     }
 }
